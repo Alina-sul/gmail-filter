13dc6a4b8dcc49987ed78571911c0ffc97de46a2
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2020-06-14 15:08:02 +0300

    -

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './App.css';
 import request from 'axios';
 import {useQuery} from 'react-query';
-import { useTable } from "react-table";
+import './style.sass'
+import { Slider } from '@material-ui/core';
 
+//import { useTable } from "react-table";
 
+const getData = async () => {
+    const {data} = await request.get(
+        'http://localhost:5000/messages'
+    );
+    return data;
+};
 const decode = function(input) {
         // Replace non-url compatible chars with base64 standard chars
         input = input
@@ -24,12 +32,30 @@ const decode = function(input) {
     };
 
 
-const getData = async () => {
-  const {data} = await request.get(
-      'http://localhost:5000/messages'
-  );
-  return data;
+/* filter initial data - creating a new array suitable for further analysis */
+const arrayForAnalysis = function(array) {
+
+    const filterByName = (arr,key) => arr
+        .filter(header => header.name === key ? header : null);
+
+    return array.map( (message) => {
+         return {
+            id: message.id,
+            sender: filterByName(message.payload.headers,'From')[0].value,
+            date: new Date(filterByName(message.payload.headers,'Date')[0].value),
+            subjectLine: filterByName(message.payload.headers,'Subject')[0].value,
+            snippet: message.snippet,
+            body: message.payload.parts !== undefined ?
+                message.payload.parts.filter(
+                    (x) => x.mimeType === 'text/html' ? x : null
+                )[0].body.data : null
+
+        };
+    });
 };
+
+
+
 
 function App() {
 
@@ -38,44 +64,42 @@ function App() {
     initialData:[]
   });
 
+    const [value, setValue] = React.useState([2, 10]);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
   React.useEffect(() => {
       if (status === 'error') return console.log(error);
       if (status === 'success' && data.length > 0) {
 
-//filter initial data - creating a new array suitable for further analysis
-          const filterByName = (arr,key) => arr.filter(header => header.name === key ? header : null);
+          const messages = arrayForAnalysis(data);
+          console.log((messages[0].date - messages[99].date)/8640000);
 
-          const messages = data.reduce( (acc,message,i) => {
-              const result = {
-                  id: message.id,
-                  sender: filterByName(message.payload.headers,'From')[0].value,
-                  date: new Date(filterByName(message.payload.headers,'Date')[0].value),
-                  subjectLine: filterByName(message.payload.headers,'Subject')[0].value,
-                  snippet: message.snippet,
-                  body: message.payload.parts !== undefined ?
-                      message.payload.parts.filter(
-                          (x) => x.mimeType === 'text/html' ? x : null
-                      )[0].body.data : null
-
-              };
-             // console.log(message.payload.parts)
-
-              return acc.concat(result)
-          },[]);
-
-          console.log(data[0]);
-          console.log(messages)
-
-      }
+          };
 
   }, [status, data, error]);
 
   return (
-    <div className="App">
-        {
-           'hi'
-        }
-    </div>
+    <>
+        <div className="App">
+            <h1>Gmail Filter</h1>
+            <div id='top-section' className='top-section'>
+                <div id='table'>
+                    <h3>Time Range </h3>
+                    <Slider
+                        value={value}
+                        onChange={handleChange}
+                        valueLabelDisplay="auto"
+                        aria-labelledby="range-slider"
+                    />
+                </div>
+                <div id='distribution'>
+                    <h3>Distribution </h3>
+                </div>
+            </div>
+        </div>
+    </>
   );
 }
 
