66037b1faa620f79a646baa0d95d3a9c84fcd72a
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2020-08-12 23:13:09 +0300

    .

--- client/src/Components/reusable/Table/Headings/Headings.jsx
+++ client/src/Components/reusable/Table/Headings/Headings.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import IconButton from "@material-ui/core/IconButton";
 
 const Headings = ({data}) => {
 
     return (
         <>
+            <IconButton aria-label="delete" disabled color="primary">
+                <DeleteIcon />
+            </IconButton>
             {
                 data.map(x => {
                     return <th className={`heading-${x}`} key={`heading-${x}`}>
